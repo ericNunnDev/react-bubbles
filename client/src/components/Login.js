907288c2957d6a348990e838d9977dd4ccb447cf
lowlyHero <e.nunn1009@gmail.com>
2019-10-18 11:30:55 -0400

    Initial commit. Deps installed. auth folder created with PrivateRoute component and axiosAuth. Login form started.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [input, setInput] = useState("");
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const handleChange = e => {
+    setInput(e.target.value);
+}
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+        <h1>Login</h1>
+        <input
+          name="username"
+          type="text"
+          placeholder="Username" 
+          value={input.username}
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password" 
+          placeholder="Password"
+          value={input.password}
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+      </form>
     </>
   );
 };
